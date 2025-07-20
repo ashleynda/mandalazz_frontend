2343252e361ebb3e076a3266cec3144c814c4803
ashleynda <ashleyndabai@gmail.com>
2025-07-20 19:47:48 +0100

    saving

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,26 +1,5 @@
-// import UserDashboard from "../../component/reusables/userDashboard"
-
-// export default function DashboardPage() {
-//   return <UserDashboard />
-// }
-
-import DashboardHeader from '@/components/DashboardHeader';
-import DashboardSidebar from '@/components/DashboardSidebar';
-import DashboardContent from '@/components/DashboardContent';
-
-import { checkComponent } from '@/utils/checkComponent';
+import UserDashboard from "../../component/reusables/userDashboard"
 
 export default function DashboardPage() {
-  // Check each imported component
-  checkComponent(DashboardHeader, 'DashboardHeader');
-  checkComponent(DashboardSidebar, 'DashboardSidebar');
-  checkComponent(DashboardContent, 'DashboardContent');
-
-  return (
-    <div>
-      <DashboardHeader />
-      <DashboardSidebar />
-      <DashboardContent />
-    </div>
-  );
+  return <UserDashboard />
 }
